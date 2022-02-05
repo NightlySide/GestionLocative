561d992c4955399501fdb8e5f54bb9c1f738e57b
nightlyside <nightlyside@gmail.com>
2022-02-05 09:18:31 +0100

    Ajout de la page de connexion et bibliothèques UI

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+import styles from "../styles/App.module.css";
+
+import type { AppProps } from "next/app";
+import { DarkThemeToggle, ThemeContext } from "../components/dark_theme_toggle";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [theme, setTheme] = useState("light");
+	const rootClassName = theme === "light" ? styles.root_light : styles.root_dark;
+
+	return (
+		<ThemeContext.Provider value={{ theme, setTheme }}>
+			<div className={`${rootClassName} bp4-${theme}`}>
+				<Component {...pageProps} />
+				<DarkThemeToggle />
+			</div>
+		</ThemeContext.Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
