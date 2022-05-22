307675800a192b50f80dd5af950cac415a77efb8
nightlyside <nightlyside@gmail.com>
2022-05-22 18:53:58 +0200

    Fixing http mistake

--- frontend/src/api/ApiConfig.ts
+++ frontend/src/api/ApiConfig.ts
@@ -1,4 +1,4 @@
 export const HOST =
 	process.env.NODE_ENV === "production" ? "https://api.gestion-locative.ietananas.fr" : "http://localhost";
 export const PORT = process.env.NODE_ENV ? 80 : 1234;
-export const API_URL = `http://${HOST}:${PORT}`;
+export const API_URL = `${HOST}:${PORT}`;
