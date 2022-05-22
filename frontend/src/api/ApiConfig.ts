45c61db1bacf38ba94475ad48266bab9547387d3
nightlyside <nightlyside@gmail.com>
2022-05-22 18:48:23 +0200

    Adding dev and prod modes

--- frontend/src/api/ApiConfig.ts
+++ frontend/src/api/ApiConfig.ts
@@ -1,3 +1,3 @@
-export const HOST = "localhost";
-export const PORT = 1234;
+export const HOST = process.env.NODE_ENV === "production" ? "api.gestion-locative.ietananas.fr" : "localhost";
+export const PORT = process.env.NODE_ENV ? 80 : 1234;
 export const API_URL = `http://${HOST}:${PORT}`;
